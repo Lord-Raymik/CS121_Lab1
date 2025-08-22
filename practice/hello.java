3823a5759acfd10446529782059ab0b7ea4dfb97
Lord-Raymik <silas.johnson@bsu.edu>
2025-08-22 18:42:45 +0000

    commited hello.java to main

---
+++ practice/hello.java
@@ -0,0 +1,9 @@
+class Hello {
+	public Hello() {
+		System.out.println("Hello World!");
+	} // end hello
+	
+	public static void main(String[] args) {
+		new Hello()
+	} // end main
+} // end class def
